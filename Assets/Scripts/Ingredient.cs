f457025529b449d424557d4a24178bd30d8f4371
ben-strehl <benjaminstrehl0@gmail.com>
2023-12-04 16:13:49 -0500

    Ingredient prefabs created

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,32 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Ingredient
+public class Ingredient:MonoBehaviour
 {
-    public IngredientType type;
-    public string name;
+    /* public IngredientType type; */
+    public string IngredientName;
+    private float moveSpeed = 5f;
+    public Vector2 goalPosition { get; set; }
+
+    void Start()
+    {
+        goalPosition = (Vector2)transform.position;
+    }
+
+    void Update()
+    {
+        var step =  moveSpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards((Vector2)transform.position, goalPosition, step);
+    }
 
     public override string ToString()
     {
-        return name;
+        return IngredientName;
     }
 }
 
-public enum IngredientType
-{
-    Flour,
-    Sugar,
-    Milk,
-    Egg,
-    Frosting,
-    Chocolate,
-    Vanilla,
-    Sprinkles,
-    Cake
-}
+/* public enum IngredientType */
+/* { */
+/*     Flour, */
+/*     Sugar, */
+/*     Milk, */
+/*     Egg, */
+/*     Frosting, */
+/*     Chocolate, */
+/*     Vanilla, */
+/*     Sprinkles, */
+/*     Cake */
+/* } */
 
-public class IngredientList
-{
-    public Ingredient[] ingredientList;
-}
