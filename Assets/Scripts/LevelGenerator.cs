89f10a78f19b81b1c2bfdd53acfd0544e083ac53
ben-strehl <benjaminstrehl0@gmail.com>
2023-12-14 21:09:13 -0500

    Removed unused using to clean up build

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 //Everything in this class is static so it persists across scene changes
