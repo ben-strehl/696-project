8faa5192cff123d4cab5e093828005ccc141b868
ben-strehl <benjaminstrehl0@gmail.com>
2023-12-18 20:37:46 -0500

    Made UI scalable, fixed hint button

--- Assets/Scripts/HintButton.cs
+++ Assets/Scripts/HintButton.cs
@@ -1,10 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class HintButton : MonoBehaviour
 {
     private GameObject hintPanel;
+    private TMP_Text text;
     void Start()
     {
+        text = GetComponentInChildren<TMP_Text>();
         hintPanel = GameObject.Find("HintPanel");
         hintPanel.SetActive(false);
     }
@@ -12,8 +15,10 @@ public class HintButton : MonoBehaviour
     public void ToggleActive() {
         if(hintPanel.activeSelf){
             hintPanel.SetActive(false);
+            text.text = "Show Functions";
         } else {
             hintPanel.SetActive(true);
+            text.text = "Hide Functions";
         }
     }
 }
